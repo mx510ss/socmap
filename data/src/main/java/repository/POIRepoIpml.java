52f2fb344de352dd1f712aaf569cf4935e853fff
ShuFFel <egid773@gmail.com>
2017-10-19 14:13:02 +0300

    core and data upgrade

---
+++ data/src/main/java/repository/POIRepoIpml.java
@@ -0,0 +1,34 @@
+package repository;
+
+import entities.JPAPOIEntity;
+import entities.POI;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class POIRepoIpml implements POIRepository {
+
+    @Autowired
+    private JPAPOIRepository repository;
+
+
+    public List<POI> getByBounds(double X1, double Y1, double X2, double Y2) {
+        List<JPAPOIEntity> list = repository.getByBounds(X1, Y1, X2, Y2);
+        List<POI> resultList = new ArrayList<POI>();
+        for(JPAPOIEntity i : list){
+            POI tmp = i.toModel();
+            resultList.add(tmp);
+        }
+        return resultList;
+    }
+
+    public void save(Iterable<POI> poi) {
+        List<JPAPOIEntity> list = new ArrayList<JPAPOIEntity>();
+        for(POI i : poi){
+            JPAPOIEntity tmp = new JPAPOIEntity(i);
+            list.add(tmp);
+        }
+        repository.save(list);
+    }
+}
