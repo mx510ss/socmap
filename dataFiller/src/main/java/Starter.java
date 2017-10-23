b50f18afc7243978424fc546bdcdeabd9ae1cac6
BolkunetsAlexandr <bolkunets.alexandre.mail@gmail.com>
2017-10-23 12:06:55 +0300

    refact

--- dataFiller/src/main/java/Starter.java
+++ dataFiller/src/main/java/Starter.java
@@ -6,9 +6,12 @@ import services.SourcePOI;
 
 import java.io.IOException;
 import java.util.List;
-//(53.6, 23.7, 53.755, 24);
+
+
+
 public class Starter {
     public static void main(String[] args) throws IOException {
+        //(53.6, 23.7, 53.755, 24);
         POISourceService poiSourceService = new SourcePOI();
         List<POI> poiList = poiSourceService.buildQuery( 53.6, 23.7, 53.755, 24);
         System.out.println(Types.EDUCATION);
