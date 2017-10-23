23face6e542b933e2d790d6b8ee3aa4544af79ea
ShuFFel <egid73@mail.ru>
2017-10-23 13:03:49 +0300

    fix typo in SQL Query

--- data/src/main/java/repository/JPAPOIRepository.java
+++ data/src/main/java/repository/JPAPOIRepository.java
@@ -9,7 +9,7 @@ import entities.JPAPOIEntity;
 import java.util.List;
 
 public interface JPAPOIRepository extends JpaRepository<JPAPOIEntity, Long>  {
-	@Query("select u from Object u where (u.X >= :X1 and u.X <= :X2) and (u.Y >= :Y1 and u.Y <= :Y2) and u.Type = type")
+	@Query("select u from Object u where (u.X >= :X1 and u.X <= :X2) and (u.Y >= :Y1 and u.Y <= :Y2) and u.Type = :type")
 	List<JPAPOIEntity> getByBounds(@Param("X1") double X1, @Param("Y1") double Y1,
 								   @Param("X2") double X2, @Param("Y2") double Y2,
 								   @Param("type")POITypes type);
