bd3571cb54b73c75eabf8ddb1a827eed2be91374
BolkunetsAlexandr <bolkunets.alexandre.mail@gmail.com>
2017-10-23 12:06:39 +0300

    refactor

--- dataFiller/src/main/java/Starter.java
+++ dataFiller/src/main/java/Starter.java
@@ -9,10 +9,8 @@ import java.util.List;
 //(53.6, 23.7, 53.755, 24);
 public class Starter {
     public static void main(String[] args) throws IOException {
-     //   POISourceService poiSourceService = new SourcePOI();
-     //   List<POI> poiList = poiSourceService.buildQuery( 53.6, 23.7, 53.755, 24);
+        POISourceService poiSourceService = new SourcePOI();
+        List<POI> poiList = poiSourceService.buildQuery( 53.6, 23.7, 53.755, 24);
         System.out.println(Types.EDUCATION);
-        Converter.getListTags(Types.EDUCATION);
-        Converter.getListTags(Types.MEDICINE);
     }
 }
