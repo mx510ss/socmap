a403dccfa479acc7e92eb9794bfac1b74e828d6a
BolkunetsAlexandr <bolkunets.alexandre.mail@gmail.com>
2017-10-25 11:06:41 +0300

    added application.properties

--- dataFiller/src/main/java/com/inctinctools/marathon2017/datafiller/services/OSMSourceService.java
+++ dataFiller/src/main/java/com/inctinctools/marathon2017/datafiller/services/OSMSourceService.java
@@ -5,8 +5,8 @@ import com.inctinctools.marathon2017.datafiller.configs.TagsConfig;
 import com.inctinctools.marathon2017.datafiller.httpClient.HttpClient;
 import com.inctinctools.marathon2017.datafiller.model.ElementsList;
 import com.inctinctools.marathon2017.datafiller.parser.ListElementsParser;
-import com.inctinctools.marathon2017.datafiller.parser.TagsConfigParser;
 import entities.POI;
+import enums.POITypes;
 import services.POISourceService;
 
 import java.io.*;
@@ -16,39 +16,27 @@ import java.util.List;
 public class OSMSourceService implements POISourceService {
 
     private HttpClient httpClient;
-    private TagsConfig tagsConfig;
 
     public OSMSourceService() {
         httpClient = new HttpClient();
-        buildTagsConfig();
-    }
-
-    private void buildTagsConfig(){
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(new File(OSMSourceService.class.getResource(TagsConfig.URL_TAGS_JSON).getPath()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            tagsConfig = new TagsConfigParser(fileInputStream).parse();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<POI> buildQuery(double x1, double y1, double x2, double y2) {
         final ElementsList elementsList = new ElementsList();
-        try {
-            for (String tag : tagsConfig.getTags())
-            httpClient.request(Api.buildRequestNode(tag, x1, y1, x2, y2), new HttpClient.ResponseListner() {
-                public void onResponse(InputStream stream) {
-                    elementsList.concatList(new ListElementsParser(stream).parse());
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+       // try {
+       //     for (String tag : tagsConfig.getTags())
+       //     httpClient.request(Api.buildRequestNode(tag, x1, y1, x2, y2), new HttpClient.ResponseListner() {
+       //         public void onResponse(InputStream stream) {
+       //             elementsList.concatList(new ListElementsParser(stream).parse());
+       //         }
+       //     });
+       // } catch (IOException e) {
+       //     e.printStackTrace();
+       // }
         return elementsList.getListPOI();
+    }
+
+    public List<POI> buildQuery(double x1, double y1, double x2, double y2, POITypes type) {
+        return null;
     }
 }
